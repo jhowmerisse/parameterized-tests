020eb350732f0a0c12ebf4c296995176f11e5c91
jhowmerisse <jhow.merisse@hotmail.com>
2019-07-01 09:15:30 -0300

    Adding mount and unmount

--- src/App.test.js
+++ src/App.test.js
@@ -1,43 +1,56 @@
 import React from 'react';
 import App from './App';
+import { mount } from 'enzyme';
 
 
 describe('Test valid value input', () => {
   
   it('Expect to return false with less than 1 caractere', () => {
     const expectedValue = false;
-    const result = App.validateValue('')
+    const component  = mount(<App />);
+    const result = component.validateValue('')
     expect(result).toBe(expectedValue);
   });
 
   it('Expect to return false with more than 10 caracteres', () => {
     const expectedValue = false;
-    const result = App.validateValue('qwertyuiopdddd')
+    const component  = mount(<App />);
+    const result = component.validateValue('qwertyuiopdddd')
     expect(result).toBe(expectedValue);
+    component.unmount();
+    component.unmount();
   });
 
   it('Expect to return true with value >= 10 caracteres', () => {
     const expectedValue = true;
-    const result = App.validateValue('qwertyuiop')
+    const component  = mount(<App />);
+    const result = component.validateValue('qwertyuiop')
     expect(result).toBe(expectedValue);
+    component.unmount();
   });
 
   it('Expect to return true with value <= 1 caracteres', () => {
     const expectedValue = true;
-    const result = App.validateValue('q')
+    const component  = mount(<App />);
+    const result = component.validateValue('q')
     expect(result).toBe(expectedValue);
+    component.unmount();
   });
 
   it('Expect to return false number', () => {
     const expectedValue = false;
-    const result = App.validateValue(22)
+    const component  = mount(<App />);
+    const result = component.validateValue(22)
     expect(result).toBe(expectedValue);
+    component.unmount();
   });
 
   it('Expect to return false if value equal null', () => {
     const expectedValue = false;
-    const result = App.validateValue(null)
+    const component  = mount(<App />);
+    const result = component.validateValue(null)
     expect(result).toBe(expectedValue);
+    component.unmount();
   });
 });
 
@@ -53,8 +66,10 @@ describe('Test valid value input 2', () => {
 
   parameters.forEach((parameter) => {
     it(parameter.description, () => {
-      const result = App.validateValue(parameter.input);
+      const component  = mount(<App />);
+      const result = component.validateValue(parameter.input);
       expect(result).toBe(parameter.result);
+      component.unmount();
     });
   });
 });
